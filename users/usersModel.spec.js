7dd1d5967c72dee95f9b2821cce85c074d1458e5
Annette Odingo <odingoannette@gmail.com>
2020-01-30 13:43:50 +0000

    Write a test to ensure that user inserted to the database matches our object

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -14,5 +14,10 @@ describe('UsersModel', ()=>{
         const users = await db('users');
         expect(users).not.toHaveLength(1);
      })
+     it('ensures user inserted exists', async()=>{
+        const user = await Users.insert({ username:'admin1', password:"1234", department:"sales"})
+        expect(user).toMatchObject({username:'admin1', password:"1234", department:"sales" })
+
+     })
  })
 })
